321613aa616d6368fd236ba5a71d9f40a1682424
Aguinaldo Mendes de Oliveira junior <123522287+Aguinaldooli@users.noreply.github.com>
2023-09-17 02:15:10 -0300

    Feat:"Implementação de uma Api Rest"

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+// Importa o módulo "express", que é um framework web para Node.js.
+const express = require('express');
+
+// Importa o objeto "router" de outro arquivo chamado "routes".
+import { router } from "./routes";
+
+// Cria uma instância do aplicativo Express.
+const app = express();
+
+// Habilita o middleware express.json()
+// para processar solicitações com formato JSON.
+app.use(express.json());
+app.use(router);
+
+app.listen(3333, () => console.log("server está rodando na porta 3333"));
